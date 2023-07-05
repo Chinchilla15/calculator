51d83ece03329540e05697dbc774a28ac1d3197c
Alessandro Chinchilla <ale030401@gmail.com>
2023-07-04 18:18:49 -0600

    Added classes and DOM nodes for those clases, still working on making the functions work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const operation = document.getElementById('operation');
-const result = document.getElementById('result');
+const displayOperation = document.getElementById('operation');
+const displayResult = document.getElementById('result');
 const ac = document.getElementById('ac');
 const posneg = document.getElementById('posneg');
 const percent = document.getElementById('percent');
@@ -18,7 +18,34 @@ const five = document.getElementById('five');
 const six = document.getElementById('six');
 const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
-const nine = document.getElementById('nine')
+const nine = document.getElementById('nine');
+const buttons = document.querySelectorAll('button');
+
+const calculator = document.getElementById('body')
+const keys = document.getElementById('btnBox');
+const operation = document.querySelectorAll('.operation');
+const number = document.querySelector('.number');
+
+buttons.forEach(button =>{
+    button.addEventListener('click', e =>{
+        if(button.classList.contains('number')){
+            console.log('Number key clicked!')
+        }else if(button.classList.contains('operation')){
+            console.log('Operation key clicked!')
+        }else if(button.id == 'equal'){
+            console.log('Equal key Clicked!')
+        }else if(button.id == 'decimal'){
+            console.log('Deciaml key cliked!')
+        }else if(button.id == 'ac'){
+            console.log('Clear key clicked!')
+        }
+    });
+});
+
+
+let num1 = 0;
+let num2 = 0;
+let operator = ['+', '-', '/','*'];
 
 const add = function(a, b){
     return a + b;
@@ -38,8 +65,37 @@ const multiply = function(...args){
 
 const divition = function (a, b){
     if(a === 0 || b === 0){
-        return"Not possible!";
+        return'Not possible!';
     }
     return a / b;
 };
 
+function percentage(partialValue, totalValue){
+    let result = ((partialValue / 100) * totalValue).toFixed(2);
+    return result
+}
+
+function changeSign(){
+
+}
+
+const operate = function(a, b, operator){
+    add(a,b);
+    subtract(a,b);
+    multiply(arg);
+    divition(a,b);
+
+}
+
+//Above the line are the functions that I will use and are still testing
+//-----------------------------------------------
+//Bellow the line is the testing area, here I will shoot a lot of code to test different ideas to make the proyect work
+
+ac.addEventListener('click', ()=>{
+    displayOperation.innerHTML = '';
+    displayResult.innerHTML = '';
+})
+
+// equal.addEventListener('click', ()=>{
+//     operate();
+// })
